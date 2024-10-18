8e52db1f43013c008ee8293fe41c80936db97f27
Люльчак Александр Олегович <lyulchak.ao@mmk.ru>
2024-10-18 12:48:02 +0500

    AddBinary

---
+++ CodeWars/SevenQ/BinaryAddition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.SevenQ
+{
+    public static class Kata
+    {
+        public static string AddBinary(int a, int b)
+        {
+            return Convert.ToString(a + b,2);
+        }
+    }
+}
