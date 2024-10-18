a88c5d2d8887fce420dd1fab8afad3145f25a6e5
Люльчак Александр Олегович <lyulchak.ao@mmk.ru>
2024-10-18 17:09:13 +0500

    GetMiddle

---
+++ CodeWars/SevenQ/GetMiddle.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeWars.SevenQ;
+
+public class GetMiddle1
+{
+    public static string GetMiddle(string s)
+    {
+        return s.Length > 1 ? s.Length % 2 == 0 ? s.Substring(s.Length / 2-1, 2) : s[s.Length / 2].ToString() : s;
+    }
+}
