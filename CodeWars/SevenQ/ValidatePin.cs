510fc55b632bf233ab791317a4ffbc0a6eb1310c
Люльчак Александр Олегович <lyulchak.ao@mmk.ru>
2024-10-18 13:28:55 +0500

    ValidatePin

---
+++ CodeWars/SevenQ/ValidatePin.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace CodeWars.SevenQ;
+
+public class ValidatePin11
+{
+    public static bool ValidatePin(string pin)
+    {
+        return (pin.Length == 4 || pin.Length == 6) && !pin.Contains('-') && !pin.Contains('+') && !pin.Contains("\n") && Int32.TryParse(pin, out int res);
+    }
+}
+//       return pin.All(n => Char.IsDigit(n)) && (pin.Length == 4 || pin.Length == 6);
