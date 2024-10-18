80c9881e00876e04ac5d6b135f5f3583dd20383d
Люльчак Александр Олегович <lyulchak.ao@mmk.ru>
2024-10-18 12:40:53 +0500

    List Filtering

---
+++ CodeWars/SevenQ/List Filtering.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeWars.SevenQ;
+
+public class ListFilterer
+{
+    public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+    {
+        List<int> result = new();
+
+        foreach (var item in listOfItems)
+            if (item is int)
+                result.Add(Convert.ToInt32(item));
+
+        return result;
+    }
+
+    //best
+    //public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+    //{
+    //    return listOfItems.OfType<int>();
+    //}
+}
---
+++ ForBuild/Program.cs
@@ -0,0 +1,5 @@
+﻿using CodeWars.SevenQ;
+
+var x = ListFilterer.GetIntegersFromList(new List<object>(){ 1, "a", "b", 0, 15 });
+Console.WriteLine(x);
+Console.ReadLine();
