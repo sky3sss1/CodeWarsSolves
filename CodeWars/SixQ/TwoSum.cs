233aaf74f8efd81dc132ec008c307e0d4212d2f1
Люльчак Александр Олегович <lyulchak.ao@mmk.ru>
2024-10-19 22:19:05 +0500

    TwoSum

---
+++ CodeWars/SixQ/TwoSum.cs
@@ -0,0 +1,27 @@
+﻿namespace CodeWars.SixQ;
+
+public class TwoSum11
+{   
+    public static int[] TwoSum(int[] numbers, int target)
+    {
+        var result = new int[2];
+        var firstIndex = 0;
+        foreach (var number in numbers)
+        {
+            var secondIndex = 0;
+            foreach (var num in numbers)
+            {
+                if (number + num == target && firstIndex != secondIndex)
+                {
+                    result[0] = firstIndex; 
+                    result[1] = secondIndex;
+                    break;
+                }
+                secondIndex++;
+            }
+            firstIndex++;
+        }
+        
+        return result;
+    }
+}
